bc61efaf482a0a131dff01dc34f85a71721bba94
alisdair73 <alisdair73@gmail.com>
2017-04-03 18:13:05 +1000

    EOD

---
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,17 @@
+sap.ui.controller("warranty_claim_inquiry.ext.controller.ListReportExt", {
+
+	onPrintRCTI: function(oEvent){
+		var warrantyClaimNumber = oEvent.getSource().getParent().getBindingContext().getObject().claimNumber;
+			
+		sap.m.URLHelper.redirect("/sap/opu/odata/sap/ZWTY_WARRANTY_CLAIMS_SRV/DocumentSet(ClaimNumber='" +
+			+ warrantyClaimNumber + "',DocumentType='RCTI')/$value", true);
+	},
+
+	onPrintTAG: function(oEvent){
+		var warrantyClaimNumber = oEvent.getSource().getParent().getBindingContext().getObject().claimNumber;
+			
+		sap.m.URLHelper.redirect("/sap/opu/odata/sap/ZWTY_WARRANTY_CLAIMS_SRV/DocumentSet(ClaimNumber='" +
+			+ warrantyClaimNumber + "',DocumentType='TAG')/$value", true);
+	}
+	
+});
